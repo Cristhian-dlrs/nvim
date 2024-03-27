562499dca0f18fedf1c990f7436524bf9ba74829
Cristhian-dlrs <62567122+Cristhian-dlrs@users.noreply.github.com>
2024-03-27 13:01:17 -0400

    add telescope git integration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,9 +15,16 @@ return {
 				},
 			})
 			local builtin = require("telescope.builtin")
+
+			-- file system
 			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+
+			-- git integraiton
+			vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+			vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+			vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 
 			require("telescope").load_extension("ui-select")
 		end,
